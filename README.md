71f8592d1f0bab892d6faa98f69b53cc4ec4ef94
Lofter1 <justin099@web.de>
2025-08-20 12:41:36 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,21 @@
+# CMRO2CSV
+Scrape CMRO and save in CSV
+
+## Usage
+
+```sh
+npx cmro2csv --characterId=<character-id>
+```
+```sh
+npx cmro2csv --help
+```
+
+The character ID can be found in the CMRO URL 
+
+```
+https://cmro.travis-starnes.com/character_details.php?character=26106&order_listing=1&list_type=1&limit=30
+```
+
+```
+character=26106
+```
---
+++ index.mjs
@@ -0,0 +1,210 @@
+#!/usr/bin/env node
+
+import puppeteer from 'puppeteer-extra'
+import stealthPlugin from 'puppeteer-extra-plugin-stealth'
+import fs from 'fs'
+import progress from 'cli-progress';
+import { format } from 'fast-csv';
+import { exit } from 'process';
+import yargs from 'yargs';
+
+// CLI Configuration
+const argv = yargs(process.argv.slice(2))
+    .usage('Usage: $0 <characterId> [options]')
+    .option('characterId', {
+        type: 'number',
+        description: 'ID of the character to scrape',
+        demandOption: true,
+    })
+    .option('challengeWaitTime', {
+        type: 'number',
+        description: 'Time to wait for Cloudflare challenge (ms)',
+        default: 5000,
+    })
+    .option('headless', {
+        type: 'boolean',
+        description: 'Run Puppeteer in headless mode',
+        default: false,
+    })
+    .option('outputFile', {
+        alias: 'o',
+        type: 'string',
+        description: 'Output CSV file name',
+    })    
+    .option('order_listing', {
+        type: 'number',
+        description: 'Order listing parameter from URL (1 = 616, 2 = Ultimate, 9 = Expanded, 12 = MC2, 15 = 2099)',
+    })
+    .help()
+    .parse();
+
+// Set the default for outputFile dynamically if not provided
+if (!argv.outputFile) {
+    argv.outputFile = `${argv.characterId}.csv`;
+}
+
+const { challengeWaitTime, headless, outputFile, characterId } = argv;
+
+let characterLink = `https://cmro.travis-starnes.com/character_details.php?character=${characterId}`;
+if (argv.order_listing) {
+    characterLink += `&order_listing=${argv.order_listing}`;
+}
+
+const progressFile = `.progress-${characterId}.json`;
+let currentPage = 1;
+let isInProgess = true;
+
+puppeteer.use(stealthPlugin())
+
+process.on('exit', function () {
+    if (isInProgess) {
+        fs.writeFileSync(progressFile, JSON.stringify({ page: currentPage }));
+    }
+});
+
+if (fs.existsSync(progressFile)) {
+    const progressData = JSON.parse(fs.readFileSync(progressFile));
+    currentPage = progressData.page;
+}
+
+// Scraping
+
+async function scrapeWebsite() {
+    const browser = await puppeteer.launch({ headless: headless });
+    const page = await browser.newPage();
+
+    try {
+        await navigateTo(page, characterLink);
+
+        const listPageCount = await page.evaluate(() => {
+            const select = document.querySelector('select[name="select2"]');
+            return select ? select.options.length : 0;
+        });
+
+        console.log("Found page count: " + listPageCount);
+
+        var pageProgressBar = new progress.Bar({ format: 'Pages [{bar}] | {value}/{total}' });
+        pageProgressBar.start(listPageCount, currentPage);
+
+        for (; currentPage <= listPageCount; currentPage++) {
+            await navigateTo(page, `${characterLink}&page=${currentPage}`);
+            await writeCSVFile(await scrapeListing(page), outputFile);
+            pageProgressBar.update(currentPage);
+        }
+
+        pageProgressBar.stop();
+        isInProgess = false;
+
+        if (fs.existsSync(progressFile)) {
+            fs.unlinkSync(progressFile);
+        }
+
+        console.log("\nFinished");
+    } catch (error) {
+        console.log('Error during scraping:', error);
+    } finally {
+        await browser.close();
+    }
+}
+
+async function scrapeListing(page) {
+    const elementCount = (await page.$$('.list_detail_body')).length;
+    const list = [];
+
+    for (let i = 0; i < elementCount; i++) {
+
+        const updatedElements = await page.$$('.list_detail_body');
+
+        await updatedElements[i].$eval('.list_detail_button_block a img', el => el.click());
+        await page.waitForNavigation({ waitUntil: 'networkidle0' });
+        await bypassCloudflare(page)
+
+        const heading = await page.$eval('h1', el => el.innerText);
+        const comicData = splitHeading(heading);
+        comicData.url = page.url();
+        list.push(comicData);
+
+        await page.goBack({ waitUntil: 'networkidle2' });
+    }
+
+    return list
+}
+
+function splitHeading(inputString) {
+    const separatorIndex = inputString.indexOf(':');
+
+    const firstPart = inputString.substring(0, separatorIndex).trim();
+    const secondPart = inputString.substring(separatorIndex + 1).trim();
+    return { readingOrderPosition: firstPart, title: secondPart }
+}
+
+// Navigation
+
+async function bypassCloudflare(page) {
+    let banner = (await page.$('.header_banner'));
+    if (banner === null) {
+        console.log("Bypass cloudflare")
+        await new Promise(function (resolve) {
+            setTimeout(resolve, challengeWaitTime);
+        });
+    }
+    banner = (await page.$('.header_banner'));
+    if (banner === null) {
+        console.log("Could not Bypass Cloudflare challenge. Try again later.");
+        exit();
+    }
+}
+
+async function navigateTo(page, link) {
+    await page.goto(link, { waitUntil: 'networkidle0' });
+    await bypassCloudflare(page)
+}
+
+// CSV
+
+async function writeCSVFile(arrayData, filePath) {
+    const fileExists = fs.existsSync(filePath);
+
+    let csvStream;
+    let writableStream;
+
+    if (fileExists) {
+        csvStream = format({ headers: false, objectMode: true });
+        writableStream = fs.createWriteStream(filePath, { flags: 'a', includeEndRowDelimiter: true });
+        writableStream.write("\n");
+    } else {
+        csvStream = format({ headers: true, objectMode: true });
+        writableStream = fs.createWriteStream(filePath);
+    }
+
+    csvStream.pipe(writableStream);
+
+    arrayData.forEach(row => {
+        csvStream.write(row);
+    });
+
+    csvStream.end();
+}
+
+
+
+let retry = false;
+
+    while (true) {
+        try {
+            await scrapeWebsite();
+            break; // success -> break out of loop
+        } catch (err) {
+            console.error("Fatal error:", err);
+
+            if (retry) {
+                console.error("Second failure. Exiting...");
+                exit(1);
+            } else {
+                retry = true;
+                console.log("Retrying in 5 minutes...");
+                await new Promise(res => setTimeout(res, 5 * 60 * 1000));
+            }
+        }
+    }
+exit();
